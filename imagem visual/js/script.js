f5e3cbab1ec2b7597f989e556640407e94786e2a
Whoan10 <juanvitorioclash@gmail.com>
2022-10-15 18:15:30 -0300

    .

---
+++ imagem visual/js/script.js
@@ -0,0 +1,16 @@
+
+function retrair(){
+  var dashboard = document.querySelector('.dashboard')
+  var estabe = document.querySelector('.estabe')
+  var esta = document.querySelector('.esta')
+  var estabele = document.querySelector('.estabele')
+
+
+  dashboard.classList.toggle('hide')
+  estabe.classList.toggle('item-hide')
+  esta.classList.toggle('item-hide')
+  estabele.classList.toggle('item-hide')
+
+
+
+}
